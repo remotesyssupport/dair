1a02ec176a3bc435415e33ff889267a62a9e93d7
Everett Toews <everett.toews@cybera.ca>
2011-05-06 18:00:25 -0400

    A script to test creation of volumes on all hosts

---
+++ OpenStack/admin/create-volumes-on-all-hosts.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#TODO: check for XXX and exit out if found
+
+HOSTS=`nova-manage service list | grep volume | sort | cut -f1 -d' '`
+AMI=$1
+
+for host in $HOSTS; do
+    euca-create-volume -s 1 -z nova:$host
+done
+
+echo "Waiting 10 seconds for volumes to be created"
+sleep 10
+
+euca-describe-volumes | sort -k7
+
+echo 'To terminate all instances execute [euca-describe-volumes | grep "dair," | cut -f2 | xargs -n1 euca-delete-volume]'
+
