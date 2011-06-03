47cf4f177d46405ea2e0e9f075c440bbde5025ea
Cameron Mann <camthemann@gmail.com>
2011-06-03 12:34:10 -0600

    Deletes everything except users

--- OpenStack/admin/compute-delete-project.sh
+++ OpenStack/admin/compute-delete-project.sh
@@ -5,56 +5,88 @@ if [[ $EUID -ne 0 ]]; then
 	exit 1
 fi
 
-if [ ! -n "$1" ]; then
-    "Please specify a project to delete"
-    exit 1
-fi
-
 if [ -z $EC2_ACCESS_KEY ]; then
 	echo "You need to set your cloud credentials"
 	echo "Try sourcing your novarc file"
 	exit 1
 fi
 
-PROJECT=$1
-#SAFE_PROJECT="	$PROJECT	"
+if [ ! -n "$1" ]; then
+	echo -n "Specify project to delete: "
+	read PROJECT
+else
+	PROJECT=$1
+fi
 
 # Use project admin's credentials
-EXPORTS=$(nova-manage user exports $PROJECT-admin)
+USER="$PROJECT-admin"
+
+EXPORTS=$(nova-manage user exports $USER)
 
 if [ ! $(echo "$EXPORTS" | wc -l) -eq 2 ]; then
-	echo "Bad project name or project admin is not $PROJECT-admin"
+	echo "Bad project name or admin is not $USER"
 	exit 1
+fi
+
+echo "About to delete project $PROJECT, continue? (y/N)"
+read ANSWER
+
+if [[ $ANSWER != 'y' && $ANSWER != 'Y' ]]
+	echo "Aborting..."
+	exit 0
 fi
 
 $EXPORTS
 
-# Images
-# NOTE(vish): leave public images because others may be using them
-euca-describe-images | grep "	private	" | cut -f2 | xargs -n1 euca-deregister
-sleep 2
+echo "Deleting images..."
+IMAGES=$(euca-describe-images | grep "	private	" | cut -f2)
 
-# Addresses
-#euca-describe-addresses | grep "$PROJECT" | cut -f2 | xargs -n1 euca-disassociate-address
-euca-describe-addresses | cut -f2 | xargs -n1 euca-disassociate-address
-sleep 2
-#euca-describe-addresses | grep "$PROJECT" | cut -f2 | xargs -n1 euca-release-address
-euca-describe-addresses | cut -f2 | xargs -n1 euca-release-address
+if [[ ! $IMAGES = '' ]]; then
+	echo $IMAGES | xargs -n1 euca-deregister
+fi
 
-# Volumes
-#euca-describe-volumes | grep "$SAFE_PROJECT" | cut -f2 | xargs -n1 euca-detach-volume
-euca-describe-volumes | cut -f2 | xargs -n1 euca-detach-volume
-sleep 2
-#euca-describe-volumes | grep "$SAFE_PROJECT" | cut -f2 | xargs -n1 euca-delete-volume
-euca-describe-volumes | cut -f2 | xargs -n1 euca-delete-volume
+echo "Releasing addresses..."
+ADDRESSES=$(euca-describe-addresses | cut -f2)
 
-# Instances
-#euca-describe-instances | grep "$SAFE_PROJECT" | cut -f2 | xargs euca-terminate-instances
-euca-describe-instances | cut -f2 | xargs euca-terminate-instances
-sleep 2
+if [[ ! $ADDRESSES = '' ]]; then
+	echo $ADDRESSES | xargs -n1 euca-disassociate-address
+	sleep 2
+	echo $ADDRESSES | xargs -n1 euca-release-address
+fi
 
-# Project
+echo "Deleting volumes..."
+VOLUMES=$(euca-describe-volumes | cut -f2)
+
+if [[ ! $VOLUMES = '' ]]; then
+	echo $VOLUMES | xargs -n1 euca-detach-volume
+	sleep 2
+	echo $VOLUMES | xargs -n1 euca-delete-volume
+fi
+
+echo "Terminating instances..."
+INSTANCES=$(euca-describe-instances | grep "INSTANCE" | cut -f2)
+
+if [[ ! $INSTANCES = '' ]]; then
+	echo $INSTANCES | xargs -n1 euca-terminate-instances
+fi
+
+echo "Deleting keypairs..."
+KEYPAIRS=$(euca-describe-keypairs | cut -f2)
+
+if [[ ! $KEYPAIRS = '' ]]; then
+	echo $KEYPAIRS | xargs -n1 euca-delete-keypair
+fi
+
+# Currently unsupported in OpenStack
+#echo "Deleting snapshots..."
+#SNAPSHOTS=$(euca-describe-snapshots | cut -f2)
+
+#if [[ ! $SNAPSHOTS = '' ]]; then
+#	echo $SNAPSHOTS | xargs -n1 euca-delete-snapshot
+#fi
+
 nova-manage project delete $PROJECT
-
-# Security groups and network
 nova-manage project scrub $PROJECT
+nova-manage user delete $USER
+
+echo "Project $PROJECT deleted.  Please note that user accounts must be deleted manually."
