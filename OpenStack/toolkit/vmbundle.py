81096e8a932b205f89c61470cf4ee6d429f5338b
Cameron Mann <camthemann@gmail.com>
2011-06-06 09:56:25 -0600

    Initial support for custom kernels and ramdisks

--- OpenStack/toolkit/vmbundle.py
+++ OpenStack/toolkit/vmbundle.py
@@ -13,8 +13,8 @@ import random
 
 GBs = 1024 * 1024 * 1024
 MBs = 1024 * 1024
-DEVICE_PREFIX = "/dev/vmbundle-"
-MOUNT_POINT_PREFIX = "/mnt/vmbundle-"
+DEVICE_PREFIX = "/dev/vd"
+MOUNT_POINT_PREFIX = "/mnt/vmbundle"
 DEFAULT_IMAGE_NAME = "new-image"
 DEFAULT_BUCKET_NAME = "my-bucket"
 
@@ -24,24 +24,33 @@ volume_mounted = False
 
 @atexit.register
 def cleanup():
-    print("\n***** Cleaning up *****")
-    if volume_mounted:
-        utils.execute("umount " + mount_point)
-    if volume_created:
-        vmcreate.detach_and_delete_volume(volume)
-    if mount_point_created:
-        utils.execute("rm -rf " + mount_point)
+	print("\n***** Cleaning up *****")
+	if volume_mounted:
+		utils.execute("umount " + mount_point)
+	if volume_created:
+		vmcreate.detach_and_delete_volume(volume)
+	if mount_point_created:
+		utils.execute("rm -rf " + mount_point)
 
 def rand_suffix():
-	return ''.join(random.sample(string.letters, 4))
+	return ''.join(random.sample(string.lowercase, 2))
 
 
 if not vminit.isRoot():
-    print("You need to run this script as root to bundle a VM.")
-    exit(1)
+	print("You need to run this script as root to bundle a VM.")
+	exit(1)
 
 custom_image_name = raw_input("Image name (%(DEFAULT_IMAGE_NAME)s): " % locals())
 custom_bucket_name = raw_input("\nBucket name (%(DEFAULT_BUCKET_NAME)s): " % locals())
+custom_kernel_path = raw_input("\nKernel path (leave blank unless you have a custom kernel): ")
+custom_ramdisk_path = raw_input("\nRamdisk path (leave blank unless you have a custom ramdisk): ")
+
+if custom_kernel_path and not os.path.exists(custom_kernel_path):
+	print("%(custom_kernel_path)s does not exist" % locals())
+	exit(1)
+if custom_ramdisk_path and not os.path.exists(custom_ramdisk_path):
+	print("%(custom_ramdisk_path)s does not exist" %locals())
+	exit(1)
 
 image_name = custom_image_name if custom_image_name else DEFAULT_IMAGE_NAME
 bucket_name = custom_bucket_name if custom_bucket_name else DEFAULT_BUCKET_NAME
@@ -57,54 +66,61 @@ instance = vmcreate.get_instance(instance_id)
 kernel_id = metadata['kernel-id']
 ramdisk_id = metadata['ramdisk-id']
 
-mount_point = MOUNT_POINT_PREFIX + rand_suffix()
+mount_point = MOUNT_POINT_PREFIX
 
 while os.path.exists(mount_point):
-    mount_point = MOUNT_POINT_PREFIX + rand_suffix()
+	mount_point = MOUNT_POINT_PREFIX + rand_suffix()
 
 print("\n***** Creating mount point %(mount_point)s *****" % locals())
 utils.execute("mkdir -p %(mount_point)s" % locals())
 mount_point_created = True
 
+# Attach volume for bundling if low on space
 if fs.f_bfree < fs.f_blocks / 2:
-    device = DEVICE_PREFIX + rand_suffix()
+	device = DEVICE_PREFIX + rand_suffix()
 
-    while os.path.exists(device):
-        device = DEVICE_PREFIX + rand_suffix()
+	while os.path.exists(device):
+		device = DEVICE_PREFIX + rand_suffix()
 
-    print("\n***** Create and attach volume to %(device)s *****" % locals())
-    volume = vmcreate.create_and_attach_volume(disk_size_in_GBs, instance, device)
-    print(volume.attachment_state())
+	print("\n***** Creating and attaching volume to %(device)s *****" % locals())
+	volume = vmcreate.create_and_attach_volume(disk_size_in_GBs, instance, device)
+	print(volume.attachment_state())
  
-    if not volume:
-        print("Error creating volume")
-        exit(1)
-   
-    volume_created = True
+	volume_created = True
 
-    if not os.path.exists(device):
-        print("Error attaching volume")
-        exit(1) 
+	if not os.path.exists(device):
+		print("Error attaching volume")
+		exit(1) 
 
-    print("\n***** Making filesystem on volume *****")
-    utils.execute("mke2fs -q -t ext3 %(device)s" % locals())
+	print("\n***** Making filesystem on volume *****")
+	utils.execute("mke2fs -q -t ext3 %(device)s" % locals())
 
-    print("\n***** Mounting volume to %(mount_point)s *****" % locals())
-    utils.execute("mount %(device)s %(mount_point)s" % locals())
+	print("\n***** Mounting volume to %(mount_point)s *****" % locals())
+	utils.execute("mount %(device)s %(mount_point)s" % locals())
 
-    volume_mounted = True
+	volume_mounted = True
 
-dirs_to_exclude = "%(mount_point)s,/root/.ssh,/ubuntu/.ssh" % locals()
-
+dirs_to_exclude = "/mnt,/tmp,/root/.ssh,/ubuntu/.ssh" % locals()
 print("\n***** Excluding directories %(dirs_to_exclude)s *****" % locals())
 
-print("\n***** Bundling volume *****")
+if custom_kernel_path:
+	kernel_name = custom_kernel_path.split('/')[-1] + '.manifest/xml'
+	utils.execute("euca-bundle-image -i %(custom_kernel_path)s -d %(mount_point)s --kernel true" % locals())
+	utils.execute("euca-upload-bundle -b %(bucket_name)s -m %(mount_point)s/%(kernel_name)s" % locals())
+	kernel_id = utils.execute("euca-register %(bucket_name)s/%(kernel_name)s" % locals())[0].split()[1]
+if custom_ramdisk_path:
+	ramdisk_name = custom_ramdisk_path.split('/')[-1]
+	utils.execute("euca-bundle-image -i %(custom_ramdisk_path)s -d %(mount_point)s --ramdisk true" % locals())
+	utils.execute("euca-upload-bundle -b %(bucket_name)s -m %(mount_point)s/%(ramdisk_name)s" % locals())
+	ramdisk_id = utils.execute("euca-register %(bucket_name)s/%(ramdisk_name)s" % locals())[0].split()[1]
+
+print("\n***** Bundling filesystem *****")
 kernel_opt = '' if kernel_id == '' else '--kernel ' + kernel_id
 ramdisk_opt = '' if ramdisk_id == '' else '--ramdisk ' + ramdisk_id
 utils.execute("euca-bundle-vol --no-inherit %(kernel_opt)s %(ramdisk_opt)s -d %(mount_point)s -r x86_64 -p %(image_name)s -s %(disk_size_in_MBs)s -e %(dirs_to_exclude)s" % locals())
 
-print("\n***** Uploading bundle *****")
+print("\n***** Uploading filesystem bundle *****")
 utils.execute("euca-upload-bundle -b %(bucket_name)s -m %(mount_point)s/%(image_name)s.manifest.xml" % locals())
 
-print("\n***** Registering bundle *****")
+print("\n***** Registering filesystem bundle *****")
 utils.execute("euca-register %(bucket_name)s/%(image_name)s.manifest.xml" % locals())
