c1cd7a51d9b344867a696322bfec93c62099f8b4
Cameron Mann <camthemann@gmail.com>
2011-06-07 15:27:56 -0600

    Deletes users

--- OpenStack/admin/compute-delete-project.sh
+++ OpenStack/admin/compute-delete-project.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Deletes given project and releases all associated resources
+# Currently deletes all associated users regardless of whether they are part of another project
+# Must be run from the management node
+
+NOVA_CONF='/etc/nova/nova.conf'
+
 if [[ $EUID -ne 0 ]]; then
 	echo "This script must be run as root"
 	exit 1
@@ -41,14 +47,14 @@ EC2_ACCESS_KEY=$EC2_ACCESS_KEY:$PROJECT
 echo "Deleting images..."
 IMAGES=$(euca-describe-images | grep "	private	" | cut -f2)
 
-if [[ ! $IMAGES = '' ]]; then
+if [[ $IMAGES != '' ]]; then
 	echo $IMAGES | xargs -n1 euca-deregister
 fi
 
 echo "Releasing addresses..."
 ADDRESSES=$(euca-describe-addresses | cut -f2)
 
-if [[ ! $ADDRESSES = '' ]]; then
+if [[ $ADDRESSES != '' ]]; then
 	echo $ADDRESSES | xargs -n1 euca-disassociate-address
 	sleep 2
 	echo $ADDRESSES | xargs -n1 euca-release-address
@@ -57,7 +63,7 @@ fi
 echo "Deleting volumes..."
 VOLUMES=$(euca-describe-volumes | cut -f2)
 
-if [[ ! $VOLUMES = '' ]]; then
+if [[ $VOLUMES != '' ]]; then
 	echo $VOLUMES | xargs -n1 euca-detach-volume
 	sleep 2
 	echo $VOLUMES | xargs -n1 euca-delete-volume
@@ -66,14 +72,14 @@ fi
 echo "Terminating instances..."
 INSTANCES=$(euca-describe-instances | grep "INSTANCE" | cut -f2)
 
-if [[ ! $INSTANCES = '' ]]; then
+if [[ $INSTANCES != '' ]]; then
 	echo $INSTANCES | xargs -n1 euca-terminate-instances
 fi
 
 echo "Deleting keypairs..."
 KEYPAIRS=$(euca-describe-keypairs | cut -f2)
 
-if [[ ! $KEYPAIRS = '' ]]; then
+if [[ $KEYPAIRS != '' ]]; then
 	echo $KEYPAIRS | xargs -n1 euca-delete-keypair
 fi
 
@@ -85,6 +91,17 @@ fi
 #	echo $SNAPSHOTS | xargs -n1 euca-delete-snapshot
 #fi
 
+echo "Deleting users..."
+LDAP_ADDR=$(sed -n 's/--ldap_url=ldap:\/\/\(.\+\)/\1/p' $NOVA_CONF)
+LDAP_USER=$(sed -n 's/--ldap_user_dn=\(.\+\)/\1/p' $NOVA_CONF)
+LDAP_PW=$(sed -n 's/--ldap_password=\(.\+\)/\1/p' $NOVA_CONF)
+LDAP_SEARCHBASE="ou=Group,$(echo $LDAP_USER | grep -o 'dc=.\+')"
+USERS=$(ldapsearch -b "cn=$PROJECT,$LDAP_SEARCHBASE" -D $LDAP_USER -h $LDAP_ADDR -xw $LDAP_PW | sed -n 's/member: uid=\([^,]\+\).\+/\1/p' | grep -xv $USER | sort | uniq)
+
+if [[ $USERS != '' ]]; then
+	echo $USERS | xargs -n1 nova-manage user delete
+fi
+
 echo "Deleting project..."
 nova-manage project delete $PROJECT
 nova-manage project scrub $PROJECT
@@ -92,4 +109,4 @@ nova-manage project scrub $PROJECT
 echo "Deleting project admin..."
 nova-manage user delete $USER
 
-echo "Project $PROJECT deleted.  Please note that user accounts must be deleted manually."
+echo "Project $PROJECT deleted."
