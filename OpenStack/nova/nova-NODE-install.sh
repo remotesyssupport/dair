2876e85d8bf257f2dfde6e1c6ebd7cf2deaa1bba
canarie <dair-support@canarie.ca>
2011-06-29 14:17:19 -0600

    Updated node install script to also work with Ubuntu post-Maverick

--- OpenStack/nova/nova-NODE-install.sh
+++ OpenStack/nova/nova-NODE-install.sh
@@ -108,7 +108,7 @@ fi
 
 apt-get -q update
 apt-get -q -y install ntp python-memcache python-mysqldb ntp
-apt-get -q -y install nova-compute nova-volume
+apt-get -q -y -t maverick install nova-compute nova-volume
 
 echo "ENABLED=1" > /etc/default/nova-common
 
