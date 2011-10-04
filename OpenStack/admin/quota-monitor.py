75fe4c6cf9032df15a9a4bfc3c251464711bc826
Andrew Nisbet <andrew.nisbet@cybera.ca>
2011-10-04 16:34:13 -0600

    SQL queries defined major classes as well.

--- OpenStack/admin/quota-monitor.py
+++ OpenStack/admin/quota-monitor.py
@@ -8,14 +8,63 @@
 # Version:  0.1 - Dev. Not ready for initial release. 
 #################################################################
 
+import os			# for getcwd()
 import sys
 import getopt       # Command line processing.
-import string
+import string		# for atoi()
+import subprocess	# for execute_call()
 
-GSTD_QUOTA_FILE = "./baseline_quotas.txt" # Gold standard quotas for baseline.
-QUOTA_FILE = "./quotas.tmp" # Saved state on condition of quotas since last run
-PROJECT_QUOTAS = {}
+APP_DIR = os.getcwd() + '/'
+GSTD_QUOTA_FILE = APP_DIR + "baseline_quotas.txt" # Gold standard quotas for baseline.
+RUNG_QUOTA_FILE = "/tmp/quotas.tmp" # Saved state on condition of quotas since last run
+NOVA_CONF = "/home/cybera/dev/nova.conf" # nova.conf -- change for production.
 
+
+# sql queries:
+# total instances by project:
+# 		select project_id, count(state_description) from instances where state_description='running' group by project_id;
+# total size of all volumes by project gigbytes:
+# 		select project_id, sum(size) from volumes where attach_status='attached' group by project_id;
+# total volumes by project:
+#		select project_id, count(size) from volumes where attach_status='attached' group by project_id;
+# Total number of floating_ips in use by a project (make sure you get rid of the NULL project value):
+# 		select project_id, count(deleted) from floating_ips where deleted=0 group by project_id;
+# CPUs in use by a project:
+# 		select project_id, sum(vcpus) from instances where state_description='running' group by project_id;
+
+
+class Zones:
+	def __init__(self):
+		self.regions = {}
+		self.password = None
+		# this requires two greps of the nova.conf file but could be done in one.
+		# get the regions like: --region_list=alberta=208.75.74.10,quebec=208.75.75.10
+		results = self.execute_call("grep region_list " + NOVA_CONF)
+		results = results.split('region_list=')[1]
+		# now split on the regions separated by a ','
+		results = results.split(',')
+		for result in results:
+			name_value = result.split('=')
+			self.regions[name_value[0].strip()] = name_value[1].strip() # gets rid of nagging newline
+		# now the password --sql_connection=mysql://root:xxxxxxxxxxxxx@192.168.2.10/nova
+		results = self.execute_call("grep sql_connection " + NOVA_CONF)
+		self.password = results.split('root:')[1].split('@')[0] # yuck.
+		#print self.password, self.regions
+		
+	def execute_call(self, command_and_args):
+		""" returns the stdout of a Unix command """
+		cmd = command_and_args.split()
+		if len(cmd) < 1:
+			return "<no cmd to execute>"
+		process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		return process.communicate()[0]
+		
+	def get_password(self):
+		return self.password
+		
+	def get_regions(self):
+		return self.regions
+		
 # metadata_items: 128
 # gigabytes: 100
 # floating_ips: 5
@@ -26,11 +75,15 @@ PROJECT_QUOTAS = {}
 # Open the qotas for each project. This is a gold standard file since once this
 # script runs the quotas are dynamically changed as over time.
 
-class ProjectQuota:
-
+class Quota:
+	""" 
+		This class represents a project's quotas. It also includes a flag to 
+		determine if the project stackholders have been alerted to overquotas.
+	"""
 			
-	def __init__(self, meta=128, Gb=1000, f_ips=10, insts=10, vols=10, cors=10):
+	def __init__(self, flags=0, meta=128, Gb=100, f_ips=10, insts=10, vols=10, cors=20):
 		self.quota = {}
+		self.quota['flags'] = flags # flags is currently if we have issued an email yet
 		self.quota['metadata_items'] = meta
 		self.quota['gigabytes'] = Gb
 		self.quota['floating_ips'] = f_ips
@@ -41,7 +94,8 @@ class ProjectQuota:
 	# reads lines from a file and if the values are '=' separated it will assign the named quota the '=' value.
 	# WARNING: If the quota name is spelt incorrectly the default value is used.
 	def set_values(self, values):
-		project_name = ""
+		""" This method is used for parsing a string of 'name=value,' quotas. """
+		project_name = None
 		for value in values:
 			name = value.split('=')[0].strip()
 			vs = value.split('=')[1].strip()
@@ -51,20 +105,44 @@ class ProjectQuota:
 				vs = string.atoi(vs)
 				self.quota[name] = vs # possible to set a quota that doesn't exist by misspelling its name
 			print "name = " + name + " value = " + str(vs)
-		PROJECT_QUOTAS[project_name] = self
+		if project_name == None or project_name == "":
+			# project name not specified in file throw exception
+			raise QuotaException("quotas found for a project with no name.")
+		else:
+			return project_name
+			
+	def set_quota(self, name, value):
+		""" Stores a quota value. """
+		self.quota[name] = value
+		
+	def get_quota(self, name):
+		""" Returns a quota value or None if not found. """
+		return self.quota[name]
 		
 	def __str__(self):
-		return repr("metadata_items: %d, gigabytes: %d, floating_ips: %d, instances: %d, volumes: %d, cores: %d" % \
-		(self.quota['metadata_items'],
+		return repr("flags: %d, metadata_items: %d, gigabytes: %d, floating_ips: %d, instances: %d, volumes: %d, cores: %d" % \
+		(self.quota['flags'],
+		self.quota['metadata_items'],
 		self.quota['gigabytes'],
 		self.quota['floating_ips'],
 		self.quota['instances'],
 		self.quota['volumes'],
 		self.quota['cores']))
 		
-# The baseline quota file should look like this:
-# project=name,
-def read_baseline_quota_file():
+# The baseline quota file is made up of lines of quotas where the minimum entry
+# is the project name. There is no maximum entry so future quotas can be added
+# with no impact to reading the file, unrecognized quotas are ignored, those
+# that are not mentioned are set to the OpenStack defaults set by compute--
+# project-create.sh. The format of the file is a single line per project with
+# name value pairs separated by ','. Trailing ',' are not permitted. Examples:
+# name_0=value_0, name_1=value_1
+# Blank lines are allowed and the comment character is '#'. 
+#
+# Valid entries tested include:
+# project=a,flags=1,metadata_items=120,gigabytes=99, floating_ips=9,   instances=4,	volumes =  2, cores    =   20
+# flags=0,metadata_items=121,gigabytes=10, floating_ips=10,   instances=5,	volumes =4, cores=7,project=z
+# cores=999, project=p
+def read_baseline_quota_file(quotas):
 	try:
 		f = open(GSTD_QUOTA_FILE)
 	except:
@@ -72,24 +150,42 @@ def read_baseline_quota_file():
 	
 	line_no = 0
 	for line in f.readlines():
-		read_values = line.split(',') # read_values holds name=value pairs.
-		line_no = line_no + 1
-		pq = ProjectQuota()
-		#try:
-		pq.set_values(read_values)
-		#except:
-		#	raise QuotaException("Malformation on line %d" % (line_no))
-		print pq
-	return True
+		if line.strip() != "" and line[0] != "#":
+			read_values = line.split(',') # read_values holds name=value pairs.
+			line_no = line_no + 1
+			project_quota = Quota()
+			name = None
+			try:
+				name = project_quota.set_values(read_values)
+			except:
+				raise QuotaException("Malformed quota file on line %d" % (line_no))
+			# save the project's quotas from file
+			quotas[name] = project_quota
+		print quotas
 		
-def reset_quotas():
+# There has to be a way to reset the quotas to the baseline for all groups 
+# in the case that there is a problem and the quotas get out of synch.
+# This method does that.
+def reset_quotas(quotas):
+	""" 
+	Reads the gold standard project quotas for projects and sets those
+	values in each zone. This function does not balance zone quotas.
+	"""
 	print "resetting quotas..."
-	read_baseline_quota_file()
-		
-	return 0
+	read_baseline_quota_file(quotas)
+	return set_quotas(quotas)
+	
+# Sets the quotas for a zone based on the supplied quotas.
+def set_quotas(zone, quotas):
+	pass
+	
+def balance_quotas(quotas):
+	#read_previous_quotas(quotas)
+	zoneData = Zones() # this will now contain the regions and sql password
+	
 
 def usage():
-  return """
+	return """
 Usage: quota-monitor.py [-hr, --help, --reset]
 
     This script is meant to be run routinely by cron to check and 
@@ -121,22 +217,27 @@ class QuotaException:
 
 
 def main():
+	
+	quotas = {}
     # parse command line options
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hr", ["help","reset"])
-    except getopt.GetoptError, err:
-        print str(err)
-        print usage()
-        sys.exit(2)
-    verbose = False
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print usage()
-            return 1
-        elif o in ("-r", "--reset"):
-			return reset_quotas()
-        else:
-            assert False, "unimplemented option '" + o + "'"
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hrb", ["help","reset","balance"])
+	except getopt.GetoptError, err:
+		print str(err)
+		print usage()
+		sys.exit(2)
+
+	
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			print usage()
+			return 1
+		elif o in ("-r", "--reset"):
+			return reset_quotas(quotas)
+		elif o in ("-b", "--balance"):
+			return balance_quotas(quotas)
+		else:
+			assert False, "unimplemented option '" + o + "'"
 
 
 if __name__ == "__main__":
