6bd923ca64b5d8b8b93bd030413f530c135006fb
Andrew Nisbet <andrew.nisbet@cybera.ca>
2011-10-26 09:55:56 -0600

    Try vm-toolkit bundle execute() for setting quotas.

--- OpenStack/admin/quota-monitor.py
+++ OpenStack/admin/quota-monitor.py
@@ -25,6 +25,14 @@ DELINQUENT_FILE = APP_DIR + "Quota-monitor_scratch.tmp" # list of delinquent pro
 #NOVA_CONF = "/home/cybera/dev/nova.conf" # nova.conf -- change for production.
 NOVA_CONF = "/etc/nova/nova.conf" # nova.conf
 
+class ProcessExecutionError(IOError):
+    def __init__(self, stdout=None, stderr=None, exit_code=None, cmd=None, description=None):
+        if description is None:
+            description = "Unexpected error while running command."
+        if exit_code is None:
+            exit_code = '-'
+        message = "%(description)s\nCommand: %(cmd)s\nExit code: %(exit_code)s\nStdout: %(stdout)r\nStderr: %(stderr)r" % locals()
+        IOError.__init__(self, message)
 
 class QuotaLogger:
 	"""Logs events of interest.
@@ -174,6 +182,35 @@ class ZoneQueryManager:
 		process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		return process.communicate()[0]
 		
+	######################################################
+	def __execute__(self, cmd, process_input=None, addl_env=None, check_exit_code=True, attempts=1):
+		while attempts > 0:
+			attempts -= 1
+			try:
+				env = os.environ.copy()
+				if addl_env:
+					env.update(addl_env)
+				obj = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+				result = None
+				if process_input != None:
+					result = obj.communicate(process_input)
+				else:
+					result = obj.communicate()
+				obj.stdin.close()
+				if obj.returncode:
+					if check_exit_code and obj.returncode != 0:
+						(stdout, stderr) = result
+						raise ProcessExecutionError(exit_code=obj.returncode, stdout=stdout, stderr=stderr, cmd=cmd)
+				return result
+			except ProcessExecutionError:
+				if not attempts:
+					raise
+				else:
+					time.sleep(5)
+
+
+	######################################################
+		
 	def get_zone_resource_snapshots(self):
 		"""
 		Run queries of resources being used in each zone
@@ -264,7 +301,7 @@ class ZoneQueryManager:
 		### PRODUCTION CODE ###
 		euca_cmd = 'ssh -o StrictHostKeyChecking=no ' + address + " \"nova-manage project quota " + quota.get_project_name() + " " + Quota.C + " " + str(quota.get_quota(Quota.C)) + "\""
 		print "running: " + euca_cmd
-		print self.__execute_call__(euca_cmd)
+		print self.__execute__(euca_cmd)
 		euca_cmd = 'ssh -o StrictHostKeyChecking=no ' + address + " \"nova-manage project quota " + quota.get_project_name() + " " + Quota.F + " " + str(quota.get_quota(Quota.F)) + "\""
 		self.__execute_call__(euca_cmd)
 		euca_cmd = 'ssh -o StrictHostKeyChecking=no ' + address + " \"nova-manage project quota " + quota.get_project_name() + " " + Quota.G + " " + str(quota.get_quota(Quota.G)) + "\""
