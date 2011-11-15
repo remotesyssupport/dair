9e954eb4cdd590bf1da880344fb6bd06c9b00f57
Andrew Nisbet <andrew.nisbet@cybera.ca>
2011-11-15 11:27:20 -0700

    Added check for shutdown instances to count against quota as per email from Mark W.

--- OpenStack/admin/quota-monitor.py
+++ OpenStack/admin/quota-monitor.py
@@ -137,7 +137,7 @@ class ZoneInstance:
 #		select project_id, sum(vcpus) from instances where state=1 group by project_id;
 class ZoneQueryManager:
 	def __init__(self):
-		self.Q_PROJECT_INSTANCES = """ "select project_id, count(state) from instances where state=1 group by project_id;" """
+		self.Q_PROJECT_INSTANCES = """ "select project_id, count(state) from instances where state=1 or state=5 group by project_id;" """
 		self.Q_PROJECT_GIGABYTES = """ "select project_id, sum(size) from volumes where attach_status=\\"attached\\" group by project_id;" """ # returns empty set not expected '0'
 		self.Q_PROJECT_VOLUMES   = """ "select project_id, count(size) from volumes where attach_status=\\"attached\\" group by project_id;" """
 		self.Q_PROJECT_FLOAT_IPS = """ "select project_id, count(deleted) from floating_ips where deleted=0 group by project_id;" """
